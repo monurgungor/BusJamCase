ab28861305bd38a161b45119c06212642b5ddcfa
Onur Güngör <onurgungorz@icloud.com>
2025-07-14 11:32:13 +0300

    feat: layout level buttons

--- Assets/_Game/_01Dev/Scripts/UI/MainMenuPanel.cs
+++ Assets/_Game/_01Dev/Scripts/UI/MainMenuPanel.cs
@@ -9,11 +9,12 @@ namespace BusJam.UI
     public class MainMenuPanel : UIPanel
     {
         [SerializeField] private Transform levelButtonContainer;
+        [SerializeField] private RectTransform containerRect;
         [SerializeField] private Button levelButtonPrefab;
 
         private LevelManager _levelManager;
         private GameStateManager _gameStateManager;
-
+        
         [Inject]
         public void Construct(LevelManager levelManager, GameStateManager gameStateManager)
         {
@@ -45,12 +46,14 @@ namespace BusJam.UI
 
             ClearExistingButtons();
 
-            for (int i = 0; i < _levelManager.TotalLevelsCount; i++)
+            for (var i = 0; i < _levelManager.TotalLevelsCount; i++)
             {
                 CreateLevelButton(i);
             }
-        }
 
+            PositionButtonsVertically();
+        }
+        
         private void ClearExistingButtons()
         {
             foreach (Transform child in levelButtonContainer)
@@ -65,17 +68,44 @@ namespace BusJam.UI
         private void CreateLevelButton(int levelIndex)
         {
             var levelData = _levelManager.GetLevel(levelIndex);
-            if (levelData == null) return;
-
+            if (levelData == null) 
+            {
+                return;
+            }
+            
             var button = Instantiate(levelButtonPrefab, levelButtonContainer);
             var buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
             
             if (buttonText != null)
             {
-                buttonText.text = $"Level {levelIndex + 1}";
+                var levelText = $"Level {levelIndex + 1}";
+                
+                buttonText.text = levelText;
             }
 
             button.onClick.AddListener(() => OnLevelButtonClicked(levelIndex));
+        }
+
+        private void PositionButtonsVertically()
+        {
+            if (levelButtonContainer == null || levelButtonContainer.childCount == 0)
+                return;
+
+            var buttonCount = levelButtonContainer.childCount;
+            var containerHeight = containerRect.rect.height;
+            var stepSize = containerHeight / (buttonCount + 1);
+            var startY = containerHeight * 0.5f;
+
+            for (var i = 0; i < buttonCount; i++)
+            {
+                var button = levelButtonContainer.GetChild(i);
+                var rectTransform = button.GetComponent<RectTransform>();
+
+                if (rectTransform == null) continue;
+                var position = rectTransform.localPosition;
+                position.y = startY - (i + 1) * stepSize;
+                rectTransform.localPosition = position;
+            }
         }
 
         private void OnLevelButtonClicked(int levelIndex)
